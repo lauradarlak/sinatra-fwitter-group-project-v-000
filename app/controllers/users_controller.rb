52350e564344beb9422ad158ddc6a7d18fef1cd8
Laura Darlak <laura@polyporedesign.com>
2018-11-02 16:39:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    puts params
     erb :'users/create_user'
   end
 
   post '/users' do
-    
+    puts params
   end
 
 end
