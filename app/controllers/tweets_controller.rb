fa010d79f1779d360ad09576550911301571a57a
Laura Darlak <laura@polyporedesign.com>
2018-11-02 19:00:18 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,6 +14,7 @@ class TweetsController < ApplicationController
   post '/tweets' do
     @tweet = Tweet.create(content: params[:content])
     @tweet.user_id = current_user
+    @tweet.save
     redirect "/tweet/#{@tweet.id}"
   end
 
