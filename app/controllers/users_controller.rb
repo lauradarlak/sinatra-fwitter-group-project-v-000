82a5c26d5c67ba90dacfde9c959d933abbfd5d92
Laura Darlak <laura@polyporedesign.com>
2018-11-02 17:41:35 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,13 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"])
-    session[:id] = @user.id
-    redirect '/tweets'
+    @user = User.find_by(username: params["username"])
+    if @user != nil && @user.password == params["password"]
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
   end
 
-  
+
 
 end
