799f59354941ed3ba86e00706cd8541424995429
Laura Darlak <laura@polyporedesign.com>
2018-11-02 19:22:17 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,27 +11,14 @@ class TweetsController < ApplicationController
     erb :'tweets/new'
   end
 
-  # post '/tweets' do
-  #   if logged_in? && params[:content].present?
-  #     user = current_user
-  #     tweet = Tweet.create(content: params[:content], user_id: params[:user_id])
-  #     tweet.user_id = user.id
-  #     tweet.save
-  #     redirect "/tweet/#{tweet.id}"
-  #   else redirect '/tweets/new'
-  #   end
-  # end
   post '/tweets' do
-
-    if logged_in? && (params[:content] != "")
-      user = User.find_by(session[:user_id])
-      tweet = Tweet.new(content: params[:content])
+    if logged_in? && params[:content].present?
+      user = current_user
+      tweet = Tweet.create(content: params[:content], user_id: params[:user_id])
       tweet.user_id = user.id
       tweet.save
-
-      redirect to "/tweets/#{tweet.id}"
-    else
-      redirect to '/tweets/new'
+      redirect "/tweet/#{tweet.id}"
+    else redirect '/tweets/new'
     end
   end
 
