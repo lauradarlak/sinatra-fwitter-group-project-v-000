40b8b8ab670e9629622fa617139b0e777a0504f1
Laura Darlak <laura@polyporedesign.com>
2018-11-02 20:17:15 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -49,7 +49,7 @@ class TweetsController < ApplicationController
 
   patch '/tweets/:id' do
     if params[:content].empty?
-      redirect '/tweets/edit_tweets'
+      redirect "/tweets/#{params[:id]}/edit_tweets"
     else
       @tweet = Tweet.find_by_id(params[:id])
       @tweet.update(content: params[:content])
