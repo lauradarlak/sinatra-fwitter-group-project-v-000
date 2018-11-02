e251d7b1f9b6c513f07cb8a94584cdfb9cee8512
Laura Darlak <laura@polyporedesign.com>
2018-11-02 17:20:16 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params["username"], password: params["password"])
+    session[:id] = @user.id
   end
 
 end
