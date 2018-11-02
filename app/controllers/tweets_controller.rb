40c8b2f4607b985b59526e16d8c1d421e8c3cec8
Laura Darlak <laura@polyporedesign.com>
2018-11-02 18:50:01 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,16 +7,20 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
   post '/tweets' do
     @tweet = Tweet.create(content: params[:content])
     redirect "/tweet/#{@tweet.id}"
   end
 
-
-
-  get '/tweets/new' do
-    erb :'tweets/new'
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show_tweets'
   end
+
 
 
 
