05193c882202807fbd1175249fc1880c4d3a6e2b
Laura Darlak <laura@polyporedesign.com>
2018-11-02 18:40:34 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       erb :'tweets/tweets'
-    else redirect '/signup'
+    else redirect '/login'
     end
   end
 
